7bee7af66f37e9ec1df78a060b6e99ea0b058f24
cardos0s <jucsantana05@icloud.com>
2025-11-10 23:44:18 -0300

    Estruturas de Dados

---
+++ ReverseString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string nome = "Julia Cardoso";
+        
+        string reverse = ReverseString(nome);
+        Console.WriteLine(reverse);
+    }
+    
+static string ReverseString(string str)
+{
+    char[] charArray = str.ToCharArray();
+    int l = 0;
+    int right = charArray.Length - 1;
+    while (l < right)
+    {
+        (charArray[l], charArray[right]) = (charArray[right], charArray[l]);
+        l++;
+        right--;
+    }
+    return new string(charArray);
+}
+
+}
